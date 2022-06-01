49db6303e8f15adc1cc51b55d30ed3aaadc2e116
viktor-medvedev <viktormedvedev2022@gmail.com>
2022-06-01 11:11:23 +0500

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+return   <nav className="deep-purple lighten-3">
+<div className="nav-wrapper">
+  <a href="#!" className="brand-logo">React Movies</a>
+  <ul id="nav-mobile" className="right hide-on-med-and-down">
+    <li><a href="!#">repo</a></li>
+
+  </ul>
+</div>
+</nav>
+}
+
+export {Header}
